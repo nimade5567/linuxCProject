98117ec8a056d7e2942ca2492b8514072d1b9c91
nimade5567 <jyh17353790598@163.com>
2026-02-09 23:18:47 +0800

    明天解决lambada函数

---
+++ test3/main.cpp
@@ -0,0 +1,23 @@
+#include<functional>
+#include<queue>
+#include<iostream>
+using namespace std;
+void task1(){
+    cout<<"Task 1 executed!"<<endl;
+    // do something
+}
+void task2(){
+    cout<<"Task 2 executed!"<<endl;
+    // do something
+}
+int main(){
+    queue<function<void()>> taskQueue;
+    taskQueue.push(task1);
+    taskQueue.push(task2);
+    while(!taskQueue.empty()){
+        function<void()> test=taskQueue.front(); // get the next task
+        test(); // execute the task
+        taskQueue.pop(); // remove the task from the queue
+    }
+    return 0;
+}
