272cf80873592abc4248bf103d56773906677a16
nimade5567 <jyh17353790598@163.com>
2026-02-11 15:08:23 +0800

    检验完毕

--- threadPoolServer/main.cpp
+++ threadPoolServer/main.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 #include "threadPool.h"
+void exampleTask(){
+    std::cout<<"the thread is running"<<std::this_thread::get_id()<<"\n"<<std::endl;
+}
 int main(){
     server::ThreadPool pool(4);
+    pool.enqueue(exampleTask);
+    pool.enqueue(exampleTask);
+    pool.enqueue(exampleTask);
+    pool.enqueue(exampleTask);
     return 0;
 }
